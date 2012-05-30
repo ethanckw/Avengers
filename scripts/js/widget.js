984a7d1b54b964053c641895eb97334a2564dedb
Fahmi Adam <mohammad@fahmi.my>
2012-05-30 11:30:02 +0800

    adding widgetlist config and design enhancement

--- scripts/js/widget.js
+++ scripts/js/widget.js
@@ -1,56 +1,133 @@
 $(document).ready(function() {
 	
-	//widgets
-	$( ".widgetElementIcons" ).draggable({
-		revert: true
-	});
+    if ($.cookie('navopen') == 1) {
+	showMenu();
+    } else {
+	hideMenu();
+    }
+
+
+
+    if (1 === 1) {
+	var abc = '<div id="trial" class="rowtype_3 ui-draggable contentRow">'+getRowHtmlString('3')+'</div>';
+	$('#contentSortable').append(abc);
+	$('#trial').children('.pc').css({'width':$('#contentSortable').width()-15, 'float':'left'});
+	updateRow();
+    }
+
+
+    //widgets
 	
-	$( ".widgetHolderRow" ).draggable({
-		connectToSortable: "#contentSortable",
-		helper: "clone",
-		revert: true
-	});
+    $( ".widgetHolderRow" ).draggable({
+	connectToSortable: "#contentSortable",
+	helper: "clone",
+	revert: true
+    });
 	
-	$("#contentSortable").sortable({
-		revert: false
-	});
+    $("#contentSortable").sortable({
+	revert: false
+    });
+
+    $('#navswitch').click(function() {
+	if ($.cookie('navopen') == null || $.cookie('navopen') == 0) {
+	    $.cookie('navopen', 1);
+	    showMenu();
+	    $('#navswitch').html("&larr;");
+	} else {
+	    $.cookie('navopen', 0);
+	    hideMenu();
+	    $('#navswitch').html("&rarr;");
+	}
+    });
 	
-	$(".left_nav").mouseenter(function(){
-		$(".widgetElementIcons").show();
-		$(".widgetHolderIcons").show();
-	});
+    $(".left_nav").mouseenter(function(){
+	if ($.cookie('navopen') == 0) {
+	    showMenu();
+	}
+    });
 	
-	$(".left_nav").mouseleave(function(){
+    $(".left_nav").mouseleave(function(){
+	if ($.cookie('navopen') == 0) {
+	    hideMenu();
+	}
+    });
+
+    //drop area
+    $('#contentSortable').droppable({
+	revert: "false",
+	accept: '.widgetHolderRow',
+	drop: function(event, ui){
+	    if(ui.helper.hasClass('widgetHolderRow')){
+		ui.draggable.removeClass('widgetHolderRow');
+		ui.draggable.addClass('contentRow');
+
+		$.cookie('navopen', 0);
+		$(".left_nav").css({'background-color':'#DDD', 'width':'8px'});
+		$(".widgetHolderIconsContainer").hide();
+		$(".widgetsContainer").hide();
 		$(".widgetElementIcons").hide();
 		$(".widgetHolderIcons").hide();
-	});
-	
-	//drop area
-	$('#contentSortable').droppable({
-		revert: "false",
-		accept: '.widgetHolderRow',
-		drop: function(event, ui){
-			if(ui.helper.hasClass('widgetHolderRow')){
-				ui.draggable.removeClass('widgetHolderRow');
-				ui.draggable.addClass('contentRow');
-				$(".widgetElementIcons").hide();
-				$(".widgetHolderIcons").hide();
-				var type = getRowType(ui.helper);
-				var new_row = getRowHtmlString(type);
 
-				ui.draggable.empty();
-				ui.draggable.html(new_row);
-			}
-			
+		var type = getRowType(ui.helper);
+		var new_row = getRowHtmlString(type);
+
+		ui.draggable.empty();
+		ui.draggable.html(new_row);
+
+		ui.draggable.children('.pc').width($('#contentSortable').width()-15).css({'float':'left'});
+
+		updateRow();
 	    }
-	});
+	}
+    });
+
+    $('.widgetHolder').droppable({
+	revert: "false",
+	accept: '.widgetElementIcons',
+	drop: function(event, ui) {
+	    if(ui.helper.hasClass('widgetElementIcons')) {
+		//alert('dropped');
+		console.log(ui);
+	    }
+	}
+    });
 	
-	$( ".widgetRow" ).sortable({
-		revert: true,
-		handle: '.drag_handle',
-		axis: 'y'
-	});
+    $( ".widgetRow" ).sortable({
+	revert: true,
+	handle: '.drag_handle',
+	axis: 'y'
+    });
 });
+
+doResize = function(type, total, mini) {
+    var size = (mini === 1)?1:10; // pixel
+    switch(type) {
+    case ('1'): // 1/1
+	return total;
+    case ('2'): // 1/2
+	return ((total-size) / 2);
+    case ('3'): // 1/3
+	return ((total-(size * 2)) / 3);
+    case ('4'): // 2/3
+	return ((((total-(size * 2)) / 3) * 2) + size);
+    }
+}
+
+updateRow = function () {
+    var total = $('#contentSortable').width()-15;
+    $('.size1of1').width(doResize('1', total, 0));
+    $('.size1of2').width(doResize('2', total, 0));
+    $('.size1of3').width(doResize('3', total, 0));
+    $('.size2of3').width(doResize('4', total, 0));
+}
+
+updateMenu = function () {
+    var total = $('.widgetHolderIconsContainer').width();
+    $('.size1of1i').width(doResize('1', total, 1));
+    $('.size1of2i').width(doResize('2', total, 1));
+    $('.size1of3i').width(doResize('3', total, 1));
+    $('.size2of3i').width(doResize('4', total, 1));
+}
 
 /*
  * This method checks which row type place holder is dragged from the 
@@ -58,64 +135,91 @@ $(document).ready(function() {
  * The xx value would be the type.
  */
 getRowType = function(element){
-	var elementIdArray = element.attr('class');
-	elementIdArray = elementIdArray.split(' ');
-	var type='';
-	for (var i in elementIdArray) {
-		if (elementIdArray[i].search('rowtype_') != -1) {
-			type = elementIdArray[i].replace('rowtype_', '');
-			return type;
-		}
+    var elementIdArray = element.attr('class');
+    elementIdArray = elementIdArray.split(' ');
+    var type='';
+    for (var i in elementIdArray) {
+	if (elementIdArray[i].search('rowtype_') != -1) {
+	    type = elementIdArray[i].replace('rowtype_', '');
+	    return type;
 	}
-	return type;
+    }
+    return type;
 }
 
 getRowHtmlString = function(type) {
-	switch (type) {
-		case ('1'):
-			return "<div class='rowtype_1 contentRow'>"+
-						"<div class='widgetHolder size1of1' ></div>"+
-						"<div class='close' >"+
-							"<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
-						"</div>"+
-					"</div>";
-		case ('2'):
-			return "<div class='rowtype_2 contentRow'>"+
-						"<div class='widgetHolder mrm size2of3' ></div>"+
-						"<div class='widgetHolder size1of3' ></div>"+
-						"<div class='close' >"+
-								"<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
-						"</div>"+
-					"</div>";	
-		case ('3'):
-			return "<div class='rowtype_3 contentRow'>"+
-						"<div class='widgetHolder mrm size1of3' ></div>"+
-						"<div class='widgetHolder size2of3' ></div>"+
-						"<div class='close' >"+
-								"<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
-						"</div>"+
-					"</div>";	
-		case ('4'):
-			return "<div class='rowtype_4 contentRow'>"+
-						"<div class='widgetHolder mrm size1of3' ></div>"+
-						"<div class='widgetHolder mrm size1of3' ></div>"+
-						"<div class='widgetHolder size1of3' ></div>"+
-						"<div class='close' >"+
-								"<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
-						"</div>"+
-					"</div>";	
-		case ('5'):
-			return "<div class='rowtype_5 contentRow'>"+
-						"<div class='widgetHolder mrm size1of2' ></div>"+
-						"<div class='widgetHolder size1of2' ></div>"+
-						"<div class='close' >"+
-								"<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
-						"</div>"+
-					"</div>";						
-	}
+    switch (type) {
+    case ('1'):
+	return "<div class='rowtype_1 contentRow pc'>"+
+	    "<div class='widgetHolder size1of1' ></div>"+
+	    "</div>"+
+	    "<div class='close' >"+
+	    "<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
+	    "</div>";
+    case ('2'):
+	return "<div class='rowtype_2 contentRow pc'>"+
+	    "<div class='widgetHolder mrm size2of3' ></div>"+
+	    "<div class='widgetHolder size1of3' ></div>"+
+	    "</div>"+
+	    "<div class='close' >"+
+	    "<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
+	    "</div>";
+    case ('3'):
+	return "<div class='rowtype_3 contentRow pc'>"+
+	    "<div class='widgetHolder mrm size1of3' ></div>"+
+	    "<div class='widgetHolder size2of3' ></div>"+
+	    "</div>"+
+	    "<div class='close' >"+
+	    "<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
+	    "</div>";
+    case ('4'):
+	return "<div class='rowtype_4 contentRow pc'>"+
+	    "<div class='widgetHolder mrm size1of3' ></div>"+
+	    "<div class='widgetHolder mrm size1of3' ></div>"+
+	    "<div class='widgetHolder size1of3' ></div>"+
+	    "</div>"+
+	    "<div class='close' >"+
+	    "<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
+	    "</div>";
+    case ('5'):
+	return "<div class='rowtype_5 contentRow pc'>"+
+	    "<div class='widgetHolder mrm size1of2' ></div>"+
+	    "<div class='widgetHolder size1of2' ></div>"+
+	    "</div>"+
+	    "<div class='close' >"+
+	    "<a class='uiCloseButton' onclick='return removeRow(this);' ></a>"+
+	    "</div>";
+    }
 };
 
 removeRow = function(removeButton) {
-	$(removeButton).parent().parent().parent().remove(); 
-	return false;
+    $(removeButton).parent().parent().remove();
+    return false;
 };
+
+showMenu = function() {
+    $(".left_nav").css({'background-color':'#FFF', 'width':'300px'});
+    $(".widgetHolderIconsContainer").show();
+    $(".widgetsContainer").show();
+    $(".widgetElementIcons").show();
+    $(".widgetHolderIcons").show();
+    $('.widgetHolderIconsContainer').css({'border':'1px dashed silver', 'padding':'5px 5px 0 5px'});
+    $('.widgetsContainer').css({
+	'border':'1px dashed silver',
+	'padding':'15px 15px 0',
+	'margin-top':'10px',
+	'height': ($('.left_nav').height() - ($('.navswitch').height() + $('.widgetHolderIconsContainer').height() + 7))
+    });
+    updateMenu();
+}
+
+hideMenu = function() {
+    $(".left_nav").css({'background-color':'#B0C4DE', 'width':'8px'});
+    $(".widgetHolderIconsContainer").hide();
+    $(".widgetsContainer").hide();
+    $(".widgetElementIcons").hide();
+    $(".widgetHolderIcons").hide();
+    $('.widgetHolderIconsContainer').css({'border':'0', 'padding':'0'});
+    $('.widgetsContainer').css({'border':'0', 'padding':'0'});
+    updateMenu();
+}
---
+++ scripts/js/widget.list.js
@@ -0,0 +1,130 @@
+var widgetIcon = [{
+    'CampaignTable' : {
+	'title': 'Data table',
+	'img' : 'report_sprite.png',
+	'imgPos': '0 113px',
+	'imgPosHover': '0px 56px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'CampaignLine' : {
+	'title': 'Line chart',
+	'img' : 'report_sprite.png',
+	'imgPos': '-66px 113px',
+	'imgPosHover': '-66px 169px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'CampaignPie' : {
+	'title': 'Pie chart',
+	'img' : 'report_sprite.png',
+	'imgPos': '-133px 113px',
+	'imgPosHover': '-133px 169px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'CampaignBar' : {
+	'title': 'Bar chart',
+	'img' : 'report_sprite.png',
+	'imgPos': '-200px 113px',
+	'imgPosHover': '-200px 169px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'CampaignChange' : {
+	'title': 'This period vs last',
+	'img' : 'report_sprite.png',
+	'imgPos': '-267px 113px',
+	'imgPosHover': '-267px 169px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'Image' : {
+	'title': 'Logo or image',
+	'img' : 'report_sprite.png',
+	'imgPos': '-334px 113px',
+	'imgPosHover': '-334px 169px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'Heading' : {
+	'title': 'Heading',
+	'img' : 'report_sprite.png',
+	'imgPos': '-401px 113px',
+	'imgPosHover': '-401px 170px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'Comment' : {
+	'title': 'Text block',
+	'img' : 'report_sprite.png',
+	'imgPos': '-468px 113px',
+	'imgPosHover': '-468px 170px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'Appendix' : {
+	'title': 'List of segments',
+	'img' : 'report_sprite.png',
+	'imgPos': '-536px 113px',
+	'imgPosHover': '-536px 170px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'CampaignScatter' : {
+	'title': 'Scatter chart',
+	'img' : 'report_sprite.png',
+	'imgPos': '-602px 113px',
+	'imgPosHover': '-602px 169px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    },
+    'CampaignLine': {
+	'id' : '',
+	'title': 'Data by period',
+	'img' : 'report_sprite.png',
+	'imgPos': '-669px 113px',
+	'imgPosHover': '-669px 169px',
+	'width' : '100%',
+	'height' : '300px',
+	'url' : 'http://kode.fahmi.my'
+    }
+}];
+
+$(document).ready(function() {
+    var html = '';
+    $.each(widgetIcon, function(k, v) {
+	$.each(v, function(k2, v2) {
+	    html += '<div '
+		+ 'id="' + k2 + '" '
+		+ 'class="widgetElementIcons" '
+		+ 'title="' + v2.title + '" '
+		+ 'style="'
+		+ 'display: block; '
+		+ 'position: relative;'
+		+ (v2.imgPos?'background: url(\'images/' + v2.img + '\') ' + v2.imgPos + ';':'background:' + v2.img + ';')
+		+ 'position: relative;'
+		+ '">'
+		+ '</div>';
+	});
+    });
+    $('#widgetsContainer').append(html);
+
+    $(".widgetElementIcons").draggable({
+	helper: 'clone',
+	revert: true
+    });
+
+    $('.frameholder').hide();
+
+});
