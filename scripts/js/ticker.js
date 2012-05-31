a7e66ee65f853db0eeaa4374bd8bd18eb12457f2
Ethan Chang <ethan@techlightenment.com>
2012-05-31 11:50:07 +0800

    Ticker Completed

---
+++ scripts/js/ticker.js
@@ -0,0 +1,33 @@
+function tick(){
+		$('#tweet_ticker li:first').slideUp( function () { $(this).appendTo($('#tweet_ticker')).slideDown(); });
+}
+
+/**
+	 * USE TWITTER DATA
+	 */
+
+	 $.ajax ({
+		 url: 'http://search.twitter.com/search.json',
+		 data: 'q=%23msgpdhackday',
+		 dataType: 'jsonp',
+		 timeout: 10000,
+		 success: function(data){
+		 	if (!data.results){
+		 		return false;
+		 	}
+
+		 	for( var i in data.results){
+		 		var result = data.results[i];
+		 		var $res = $("<li />");
+		 		$res.append('<img src="' + result.profile_image_url + '" />');
+		 		$res.append(result.text);
+
+		 		console.log(data.results[i]);
+		 		$res.appendTo($('#tweet_ticker'));
+		 	}
+			setInterval(function(){ tick() }, 1000);	
+
+			$('#tweets').show();
+
+		 }
+	});
