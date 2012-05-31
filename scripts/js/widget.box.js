aab03aa5b4d0777728eb70d0a30d7743352cd26e
Fahmi Adam <mohammad@fahmi.my>
2012-05-31 10:05:42 +0800

    widget refresh

--- scripts/js/widget.box.js
+++ scripts/js/widget.box.js
@@ -15,15 +15,15 @@ var populateWBox = function(id, obj) {
     });
 
 
-    refreshWBoxFrame(id, url);
+    createWBoxFrame(id, url);
 
     // attach action button
     $("#" + id + " .widget-action-refresh").unbind('click').click(function() { // close
-	cl('refresh');
-
+	refreshWBoxFrame(id, url);
     });
 
     $("#" + id + " .widget-action-close").unbind('click').click(function() { // close
+	// refactor fahmi?
 	$(this).parent().parent().parent().parent().parent().css('border-color', '');
 	$(this).parent().parent().parent().parent().css('border-color', '').remove();
     });
@@ -33,30 +33,35 @@ var refreshWBox = function(box) {
     box.css('border-color', '#FFF');
 }
 
-var closeWBox = function(box) {
-    box.css('border-color', '#E2E2E2')
+var closeWBox = function(box) { // not use it, 
+    box.css('border-color', '#E2E2E2');
 }
 
-var refreshWBoxFrame = function(id, url) {
+var createWBoxFrame = function(id, url) {
     var style = "width: 100%;"
 	    + "height: 100%;"
 	    + "border: 0;"
 	    + "overflow: hidden;"
 	    + "";
 
-    doWBoxLoading(id);
+    doWBoxLoading(id); // do loading
 
     $('#' + id + ' .widget-body-content').html(
 	'<object '
 	    + 'id="if-' + id + '" '
 	    + 'style="' + style + '" '
-	    + 'data="' + url  + '"'
+	    + 'data=""'
 	    + 'tyle="text/html"'
 	    + '></object>'
     );
 
+    refreshWBoxFrame(id, url); // time to refresh
+};
 
-    $('#if-' + id).ready(function() {
+var refreshWBoxFrame = function(id, url) {
+    doWBoxLoading(id); // do loading
+
+    $("#if-" + id).attr('data', url).ready(function() {
 	setTimeout(function() { // for mocking purpose
 	    $('#' + id + ' .widget-body-overlay').hide();
 	}, 2500);
