9d7bf71a38cef7951a3e50c994f1a7bb74e26c8e
Felix Tubiana <ftubiana@cheetahmail.com>
2012-05-31 00:41:59 -0400

    Fixed bug.

--- bin/gen_toyota_html.pl
+++ bin/gen_toyota_html.pl
@@ -20,7 +20,14 @@ my $cmd = 'find ' . $directory . ' | egrep js$';
 print "Generating HTML files for $directory\n";
 chomp( my @files = `$cmd` );
 
+$cmd = 'find ' . $directory . ' | egrep html$';
+print "Generating list of existing HTML files for $directory\n";
+chomp( my @html_files = `$cmd` );
+
+
+
 my $file_data = [];
+my @generated_html;
 for my $file (@files) {
     my ($fullname, $dir) = fileparse($file);
     my ($name) = $fullname =~ /(.+)\.js/;
@@ -38,16 +45,45 @@ for my $file (@files) {
         meta => $meta,
         folder => $dir || DEFAULT_FOLDER,
     };
+    push @generated_html, $html;
 }
 
+for my $file (@html_files) {
+    my ($fullname, $dir) = fileparse($file);
+    next if ($dir eq $directory . "/");
+    $dir =~ s|.+?(\w+)/$|$1|;
+    $dir =~ s|^\.\/||; # kill leading ./
+    $dir =~ s|\/$||;   # kill trailing /
+    $dir =~ s|$directory||;
+    my $html = $file;
+    next if grep {$_ eq $html} @generated_html;
+    
+    print "   ---> $html \n";
+    my $title = $fullname;
+    $title =~ s|\.html$||;
+    $title =~ s|_| |;
+    $title = ucfirst($title);
+    push @$file_data, {
+        name => $fullname,
+        file => $html,
+        meta => {
+            title => $title
+        },
+        folder => $dir || DEFAULT_FOLDER,
+    };
+}
 create_js_data($file_data);
 
 exit 0;
 
 sub create_html {
     my ($name, $file, $meta) = @_;
-    my ($fname) = $file =~ /(.+)\.js$/;
+    my ($fname) = $file =~ m|\/?(.+)\.js$|;
     my $html = "$fname.html";
+
+    my ($fullname, $dir) = fileparse($file);
+    $file = $fullname;
+    print "$file\n";
     print "   -> $html\n";
     open my $fh, ">$html" or die $!;
 
@@ -117,12 +153,14 @@ sub create_js_data {
         ];
     } keys %$h;
 
-    open my $fh, ">menu_data.js" or die $!;
+    print "Creating menu data\n";
+    my $out =  $ENV{PWD} . "/menu_data.js";
+    print "--> $out\n";
+    open my $fh, ">$out" or die $!;
     print $fh to_json($json);
     close $fh;
     1;
 }
-
 
 __END__
 
